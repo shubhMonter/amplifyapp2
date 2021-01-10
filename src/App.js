da0634f622041f8aecd74d27483dd73ea8f69363
shubham <sv.dixit10@gmail.com>
2021-01-11 01:54:01 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-function App() {
-  return (
-    <div className="App">
-       <AmplifySignOut />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import logo from './logo.svg';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+const App = () => {
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+
+    React.useEffect(() => {
+        onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+       <div className="App">
+          <AmplifySignOut />
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <div>Hello, {user.username}</div>
+         <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Learn React
+         </a>
+       </header>
+     </div>
+    ) : (
+      <AmplifyAuthenticator />
   );
 }
 
-export default withAuthenticator(App);
+export default App;
